0235a23cc15cdcd7c230840cda02bf5c61e18d58
Sakib UL Hasan <sakibulhasan159@gmail.com>
2025-08-20 17:43:14 +0600

    Fixed admin firebase config

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,15 +1,15 @@
 import admin from "firebase-admin";
-import path from "path";
-import fs from "fs";
 
 if (!admin.apps.length) {
-  const serviceAccountPath = path.join(process.cwd(), "service-account.json");
-  const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
   });
 }
+
 
 // Firestore instance
 export const db = admin.firestore();
